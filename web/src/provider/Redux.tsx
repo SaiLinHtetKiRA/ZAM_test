5d92a1040c2484d2db32aa39b4f6c51b9878fbe4
Sai LIn Htet(KiRA) <slin91244@gmail.com>
2024-08-21 21:38:05 +0630

    Update Redux.tsx

--- web/src/provider/Redux.tsx
+++ web/src/provider/Redux.tsx
@@ -1,7 +1,11 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, Suspense } from "react";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
 export default function Redux({ children }: { children: ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Suspense>
+      <Provider store={store}>{children}</Provider>
+    </Suspense>
+  );
 }
