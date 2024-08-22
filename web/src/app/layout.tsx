00d8b59f20603e59ab7d064198a65067adc5b719
Sai LIn Htet(KiRA) <slin91244@gmail.com>
2024-08-22 20:47:57 +0630

    f

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Suspense } from "react";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Redux from "@/provider/Redux";
@@ -21,10 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Redux>
-          <Suspense>
-            <Nav />
-          </Suspense>
-
+          <Nav />
           {children}
         </Redux>
       </body>
